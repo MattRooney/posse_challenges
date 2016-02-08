168c5629c617fbe39f09a6d1cb85de2e271302d1
Allan <amaxwellblair@gmail.com>
2016-02-08 08:58:11 -0700

    initial commit on codebreakers

---
+++ codebreakers/test/codebreakers_test.rb
@@ -0,0 +1,30 @@
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative 'codebreakers'
+
+class CodeBreakersTest < Minitest::Test
+  attr_reader :cipher
+
+  def setup
+    @cipher = CodeBreaker.new
+  end
+
+  def test_if_codebreakers_exists
+    assert cipher
+  end
+
+  def test_initials_for_given_name
+    # cover edge cases
+    assert_equal "TS", cipher.initials("Taylor Swift")
+  end
+
+  def test_ceasar_cipher_works_with_upper_and_lower_case_letters
+    assert_equal "Or fher gb qevax lbhe Binygvar!", cipher.caeser_cipher(13, "Be sure to drink your Ovaltine!")
+  end
+
+  def test_the_worst_works_with_upper_and_lower_case_letters
+    assert_equal "Faip DYI gppok tgytb.", ciper.the_worst("taylor", "Make PHP great again.")
+  end
+
+end
